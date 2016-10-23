8b0485e72575b5a5ab5e2c7d1cf0676a4d2ae1a7
Heron Chavez <Heron Chavez>
2016-10-23 13:26:02 -0700

    add treasury view

---
+++ app/treasury/treasury.controller.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('myApp.treasury', ['ui.router'])
+	.config(['$stateProvider', function($stateProvider){
+		$stateProvider
+			.state('treasury', {
+				url: '/treasury',
+				templateUrl: 'treasury/treasury.html',
+				controller: 'TreasuryCtrl'
+			});
+}])
+
+.controller('TreasuryCtrl', function TreasuryCtrl($scope, gameService){
+
+});
